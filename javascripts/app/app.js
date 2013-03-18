b525ee1ea47d93099cddf247d82eafadf6610228
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-03-17 23:12:34 -0400

    Add some basic styling for tabs and categories. Create tabs. Get content for the All tab to display descriptions and categories for each itemfrom all.json.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -3,7 +3,26 @@
     var $ = window.$,
         main;
     main = function () {
-      
+        var clickHandler = function (anchor) {
+            anchor.click(function () {
+                var target = $(this).attr('href');               
+                $('.active').removeClass('active');
+                $(this).addClass('active');
+                $('#' + target).addClass('active');
+                return false;
+            });
+        };
+        
+        $.getJSON('all.json', function (todos) {
+            todos.forEach(function (todo) {
+                $('#all').append('<div class="todoItem">' + todo.description + '</div>');
+                todo.categories.forEach(function (category) {
+                    $('#all').append('<p class="categories">' + category + '</p>');
+                });
+            });
+        });
+        
+        clickHandler($('.tabs .tab'));
     };
     $(document).ready(main);
     window.main = main;
