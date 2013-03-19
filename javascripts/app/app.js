41bb24836d3c71520ec923b7c9dfbc4f3ebb4fd9
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-03-19 14:28:42 -0400

    Fix syntax for htmlLint and jsLint. Begin work on Add functionality.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,37 +1,49 @@
 (function () {
     "use strict";
     var $ = window.$,
+        clickHandler,
+        removeHandler,
+        addHandler,
         main;
     main = function () {
-        var clickHandler = function (anchor) {
+        clickHandler = function (anchor) {
             anchor.click(function () {
-                var target = $(this).attr('href');               
+                var target = $(this).attr('href');
                 $('.active').removeClass('active');
                 $(this).addClass('active');
                 $('#' + target).addClass('active');
                 return false;
             });
         };
-        var removeHandler = function (button){
+        removeHandler = function (button) {
             button.click(function () {
                 $(this).parent().remove();
                 $(this).remove();
                 return false;
             });
         };
+        addHandler = function () {
+            $('#form_submit').click(function () {
+                var newToDo,
+                    newCategory;
+                newToDo = $('#description_input').val();
+                newCategory = $('#categories_input').val();
+                $('#all').append('<div class="todoItems"><button class="removeButton">X</button>' + newToDo + '<p class="categories">' + newCategory + '</p></div>');
+                removeHandler($('.removeButton'));
+            });
+        };
         $.getJSON('javascripts/lib/all.json', function (todos) {
             todos.forEach(function (todo) {
-                $('#all').append('<div class="todoItems"><button class="removeButton">X</button>' + todo.description + '<p class="categories">' + todo.categories +'</p></div>');
+                $('#all').append('<div class="todoItems"><button class="removeButton">X</button>' + todo.description + '<p class="categories">' + todo.categories + '</p></div>');
                 removeHandler($('.removeButton'));
-                todo.categories.forEach(function (category) {    
+                todo.categories.forEach(function (category) {
                     $('#' + category).append('<div class="todoItems"><button class="removeButton">X</button>' + todo.description + '</div>');
                     removeHandler($('.removeButton'));
                 });
             });
-        }); 
-
+        });
         clickHandler($('.tabs .tab'));
-        //removeHandler($('.removeButton'));
+        addHandler();
     };
     $(document).ready(main);
     window.main = main;
