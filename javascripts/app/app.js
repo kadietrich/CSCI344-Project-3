4167f6dad2c1eed315f18d73d4db0cee5a552b06
Kurt Dietrich <kurt.dietrich0801@gmail.com>
2013-03-19 13:32:03 -0400

    Add functionality to remove buttons.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -12,20 +12,24 @@
                 return false;
             });
         };
+        var removeHandler = function (button){
+            button.click(function () {
+                $(this).parent().remove();
+                $(this).remove();
+                return false;
+            });
+        };
         $.getJSON('javascripts/lib/all.json', function (todos) {
             todos.forEach(function (todo) {
                 $('#all').append('<div class="todoItems"><button class="removeButton">X</button>' + todo.description + '<p class="categories">' + todo.categories +'</p></div>');
-                todo.categories.forEach(function (category) {     
+                removeHandler($('.removeButton'));
+                todo.categories.forEach(function (category) {    
                     $('#' + category).append('<div class="todoItems"><button class="removeButton">X</button>' + todo.description + '</div>');
+                    removeHandler($('.removeButton'));
                 });
             });
         }); 
-        /*var removeHandler = function (button) {
-            button.click(function () {
-                var target = $(this).parent();
-                target.remove();
-            })
-        }*/
+
         clickHandler($('.tabs .tab'));
         //removeHandler($('.removeButton'));
     };
